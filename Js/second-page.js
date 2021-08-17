693d48322029ee42c2e09c39b88d48d423f42318
Nazarbek2007 <87320583+Nazarbek2007@users.noreply.github.com>
2021-08-17 13:05:12 +0300

    Add files via upload

---
+++ Js/second-page.js
@@ -0,0 +1,114 @@
+var two = prompt('Введите время в цифрах')
+
+two = Number(two)
+
+switch (two) {
+    case 1:
+    alert('Сейчас час ночи!')
+    break;
+    
+    case 2:
+    alert('Сейчас два часа ночи!')
+    break;
+    
+    case 3:
+    alert('Сейчас три часа ночи!')
+    break;
+    
+    case 4:
+    alert('Сейчас четыри часа утра!')
+    break;
+    
+    case 5:
+    alert('Сейчас пять часов утра!')
+    break;
+    
+    case 6:
+    alert('Сейчас шесть часов утра!')
+    break;
+    
+    case 7:
+    alert('Сейчас семь часов утра!')
+    break;
+    
+    case 8:
+    alert('Сейчас восемь часов утра!')
+    break;
+    
+    case 9:
+    alert('Сейчас девять часа утра!')
+    break;
+    
+    case 10:
+    alert('Сейчас десять часов утра!')
+    break;
+    
+    case 11:
+    alert('Сейчас одиннадцать часов дня!')
+    break;
+    
+    case 12:
+    alert('Сейчас полдень!')
+    break;
+    
+    case 13:
+    alert('Сейчас час дня!')
+    break;
+    
+    case 14:
+    alert('Сейчас два часа дня!')
+    break;
+    
+    case 15:
+    alert('Сейчас три часа дня!')
+    break;
+    
+    case 16:
+    alert('Сейчас четыри часа дня!')
+    break;
+    
+    case 17:
+    alert('Сейчас пять часов дня!')
+    break;
+    
+    case 18:
+    alert('Сейчас шесть часов вечера!')
+    break;
+    
+    case 19:
+    alert('Сейчас семь часов вечера!')
+    break;
+    
+    case 20:
+    alert('Сейчас восемь часов вечера!')
+    break;
+    
+    case 21:
+    alert('Сейчас девять часов вечера!')
+    break;
+    
+    case 22:
+    alert('Сейчас десять часов ночи!')
+    break;
+    
+    case 23:
+    alert('Сейчас одинадцать часов ночи!')
+    break;
+    
+    case 24:
+    alert('Сейчас полночь!')
+    break;
+    
+    case 00:
+    alert('Сейчас полночь!')
+    break;
+    
+    case 0:
+    alert('Сейчас полночь!')
+    break;
+    
+    default:
+        alert('ЗАПОМНИ!!! Такого времени не сушествует!!')
+        break;
+        
+}
